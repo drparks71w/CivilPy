6a642aa8f1dee95cd40884dbbfc6ce5e3c9f6787
dane <dane@daneparks.com>
2023-03-02 08:29:37 -0500

    removed strict dependency versions to attempt to get package working on python 3.6
    -no_tests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='civilpy',
-    version='0.0.29',
+    version='0.0.30',
     description='Civil Engineering Tools in Python',
     url="https://daneparks.com/Dane/civilpy",
     author_email="Dane@daneparks.com",
