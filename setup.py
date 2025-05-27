df3a6594b780c9f45b423c0c0f0fd47843d4ddc2
Dane <dane@daneparks.com>
2025-05-27 17:43:26 -0400

    Release 0.1.7 - Added OSE tools

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="civilpy",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(
         "src", exclude=["tests", "Notebooks", "secrets", "docs", "res"]
     ),
