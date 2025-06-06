87b2f34e0586258fb4ce087146da288a4ce90b4d
Dane <dane@daneparks.com>
2025-06-06 12:12:21 -0400

    Release 0.1.10 - Added AASHTO Load combinations

--- src/civilpy/structural/aashto.py
+++ src/civilpy/structural/aashto.py
@@ -75,4 +75,13 @@ class MethodABearing:
             self.checks['Steel Laminate Edge Cover Check'] = 1
 
 
+class HL93Load:
+    def __init__(self):
+        self.axels = {
+            'spacing': 6,
+            1: {'load': 8, 'dist': 0},
+            2: {'load': 32, 'dist': 14},
+            3: {'load': 32, 'dist': [14, 30]}
+        }
+
 
